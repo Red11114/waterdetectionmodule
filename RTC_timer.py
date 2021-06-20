744bd06baec372731cae68e8e76eadaaedecd492
Red11114 <56987716+Red11114@users.noreply.github.com>
2021-06-20 22:14:39 +0930

    new library

--- RTC_timer.py
+++ RTC_timer.py
@@ -3,68 +3,72 @@ import smbus
 import time
 import os
 
-bus = smbus.SMBus(1)
+ds3231 = SDL_DS3231.SDL_DS3231(1, 0x68)
 
-DS3231 = 0x68
+# bus = smbus.SMBus(1)
 
-SECONDS_REG = 0x00
-ALARM1_SECONDS_REG = 0x07
+# DS3231 = 0x68
 
-CONTROL_REG = 0x0E
-STATUS_REG = 0x0F
+# SECONDS_REG = 0x00
+# ALARM1_SECONDS_REG = 0x07
 
-def int_to_bcd(x):
-    return int(str(x)[-2:], 0x10)
+# CONTROL_REG = 0x0E
+# STATUS_REG = 0x0F
 
-def write_time_to_clock(pos, hours, minutes, seconds):
-    bus.write_byte_data(DS3231, pos, int_to_bcd(seconds))
-    bus.write_byte_data(DS3231, pos + 1, int_to_bcd(minutes))
-    bus.write_byte_data(DS3231, pos +2, int_to_bcd(hours))
+# def int_to_bcd(x):
+#     return int(str(x)[-2:], 0x10)
 
-def set_alarm1_mask_bits(bits):
-    pos = ALARM1_SECONDS_REG
-    for bit in reversed(bits):
-        reg = bus.read_byte_data(DS3231, pos)
-        if bit:
-            reg = reg | 0x80
-        else:
-            reg = reg & 0x7F
-        bus.write_byte_data(DS3231, pos, reg)
-        pos = pos + 1
+# def write_time_to_clock(pos, hours, minutes, seconds):
+#     bus.write_byte_data(DS3231, pos, int_to_bcd(seconds))
+#     bus.write_byte_data(DS3231, pos + 1, int_to_bcd(minutes))
+#     bus.write_byte_data(DS3231, pos +2, int_to_bcd(hours))
 
-def enable_alarm1():
-    reg = bus.read_byte_data(DS3231, CONTROL_REG)
-    bus.write_byte_data(DS3231, CONTROL_REG, reg | 0x05)
+# def set_alarm1_mask_bits(bits):
+#     pos = ALARM1_SECONDS_REG
+#     for bit in reversed(bits):
+#         reg = bus.read_byte_data(DS3231, pos)
+#         if bit:
+#             reg = reg | 0x80
+#         else:
+#             reg = reg & 0x7F
+#         bus.write_byte_data(DS3231, pos, reg)
+#         pos = pos + 1
 
-def clear_alarm1_flag():
-    reg = bus.read_byte_data(DS3231, STATUS_REG)
-    bus.write_byte_data(DS3231, STATUS_REG, reg & 0xFE)
+# def enable_alarm1():
+#     reg = bus.read_byte_data(DS3231, CONTROL_REG)
+#     bus.write_byte_data(DS3231, CONTROL_REG, reg | 0x05)
 
-def check_alarm1_triggered():
-    return bus.read_byte_data(DS3231, STATUS_REG) & 0x01 != 0
+# def clear_alarm1_flag():
+#     reg = bus.read_byte_data(DS3231, STATUS_REG)
+#     bus.write_byte_data(DS3231, STATUS_REG, reg & 0xFE)
 
-def set_timer(hours, minutes, seconds):
-    # zero the clock
-    write_time_to_clock(SECONDS_REG, 0, 0, 0)
-    # set the alarm
-    write_time_to_clock(ALARM1_SECONDS_REG, hours, minutes, seconds)
-    # set the alarm to match hours minutes and seconds
-    # need to set some flags
-    set_alarm1_mask_bits((True, False, False, False))
-    enable_alarm1()
-    clear_alarm1_flag()
+# def check_alarm1_triggered():
+#     return bus.read_byte_data(DS3231, STATUS_REG) & 0x01 != 0
+
+# def set_timer(hours, minutes, seconds):
+#     # zero the clock
+#     write_time_to_clock(SECONDS_REG, 0, 0, 0)
+#     # set the alarm
+#     write_time_to_clock(ALARM1_SECONDS_REG, hours, minutes, seconds)
+#     # set the alarm to match hours minutes and seconds
+#     # need to set some flags
+#     set_alarm1_mask_bits((True, False, False, False))
+#     enable_alarm1()
+#     clear_alarm1_flag()
 
 #
 # Your sensor behaviour goes here
 #
-print(time.strftime("%Y-%m-%d %H:%M:%S"))
+print("Raspberry Pi=\t" + time.strftime("%Y-%m-%d %H:%M:%S"))
+print("Ds3231=\t\t%s" % ds3231.read_datetime())
+
 os.system('date')
 os.system('sudo date -s "2016-10-16 21:00"')
 os.system('date')
 
 while(True):
     print("start")
-    set_timer(0,0,5)
+    # set_timer(0,0,5)
     print("wait 5s")
     time.sleep(5)
     print("wait another 5s")
